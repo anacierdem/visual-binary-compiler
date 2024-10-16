d20b5e7635416d59e1f89b35c7e7110f0507e913
Ali Naci Erdem <anacierdem@gmail.com>
2024-10-16 22:50:33 +0300

    Implement basic serial connection

--- src/nodes/serial.ts
+++ src/nodes/serial.ts
@@ -1,46 +1,122 @@
-import { IComboWidget, LGraphCanvas, LGraphNode, Vector2 } from "litegraph.js";
+import {
+  IButtonWidget,
+  IComboWidget,
+  INumberWidget,
+  LGraphNode,
+} from 'litegraph.js';
 
 export class Serial extends LGraphNode {
-  portSelector: IComboWidget;
+  connectButton: IButtonWidget;
+  knownPorts: SerialPort[] = [];
   constructor() {
     super();
 
-    this.portSelector = this.addWidget<IComboWidget>("combo", "PORT", [], () => {}, { values: [] } );
+    this.properties = {
+      baudRate: 9600,
+      dataBits: 8,
+      stopBits: 1,
+      parity: 'none',
+      bufferSize: 256,
+      flowControl: 'none',
+    };
 
-    navigator.serial.addEventListener("connect", (e) => {
-      console.log("CONNECT", e);
-        // Connect to `e.target` or add it to a list of available ports.
+    this.connectButton = this.addWidget<IButtonWidget>('button', '...', null);
+
+    // TODO: implement these
+    this.addWidget<INumberWidget>('number', 'baud rate', 9600, 'baudRate');
+    this.addWidget<INumberWidget>('number', 'data bits', 8, 'dataBits');
+    this.addWidget<INumberWidget>('number', 'stop bits', 1, 'stopBits');
+    this.addWidget<IComboWidget>('combo', 'parity', ['none'], () => {}, {
+      values: ['none', 'even', 'odd'],
     });
-    
-    navigator.serial.addEventListener("disconnect", (e) => {
-        // Remove `e.target` from the list of available ports.
+    this.addWidget<INumberWidget>('number', 'buffer size', 256, 'bufferSize');
+    this.addWidget<IComboWidget>('combo', 'flow control', ['none'], () => {}, {
+      values: ['none', 'hardware'],
     });
-    
+
+    this.disableConnectButton();
+
+    navigator.serial.addEventListener('connect', (e) => {
+      e.target && this.establishConnection(e.target as SerialPort);
+    });
+
     navigator.serial.getPorts().then((ports) => {
-      this.portSelector.options = { values: [] };
-      console.log("PORTS", ports);
-      // this.portSelector.options.values = ports.map((port) => port.getInfo().usbVendorId);
+      this.knownPorts = ports ?? [];
+
+      if (ports.length > 0) {
+        this.establishConnection(ports[0]);
+        return;
+      }
+
+      this.enableConnectButton();
     });
-      
   }
-  title = "Serial";
+  title = 'Serial';
 
-  onExecute() {
+  async onConnectClick() {
+    const port = await navigator.serial.requestPort().catch(() => {
+      return null;
+    });
 
+    if (!port) {
+      return;
+    }
+
+    for (const knownPort of this.knownPorts) {
+      await knownPort.close().catch(() => {});
+      await knownPort.forget();
+    }
+
+    // Currently only a single port is supported.
+    this.knownPorts = [port];
+    this.establishConnection(port);
   }
-  
-  onMouseDown(event: MouseEvent, pos: Vector2, graphCanvas: LGraphCanvas): void {
-    navigator.serial
-      .requestPort()
-      .then(async (port) => {
-        await port.open({ baudRate: 9600 });
-        console.log("OPEN", port.connected);
-        // Connect to `port` or add it to the list of available ports.
-      })
-      .catch((e) => {
-        // The user didn't select a port.
+
+  async establishConnection(port: SerialPort) {
+    try {
+      this.disableConnectButton();
+      console.log(this.properties);
+      await port.open({
+        baudRate: this.properties.baudRate,
+        bufferSize: this.properties.bufferSize,
+        dataBits: this.properties.dataBits,
+        stopBits: this.properties.stopBits,
+        parity: this.properties.parity,
+        flowControl: this.properties.flowControl,
       });
+      this.enableConnectButton('Disconnect', this.disconnect.bind(this, port));
+    } catch (e) {
+      this.enableConnectButton('Failed to connect');
+      console.error(e);
+    }
   }
+
+  async disconnect(port: SerialPort) {
+    try {
+      this.disableConnectButton();
+      await port.close();
+      this.enableConnectButton();
+    } catch (e) {
+      this.enableConnectButton(
+        'Failed to disconnect',
+        this.disconnect.bind(this, port)
+      );
+      console.error(e);
+    }
+
+    this.knownPorts = [];
+    this.enableConnectButton();
+  }
+
+  disableConnectButton(name?: string) {
+    this.connectButton.callback = undefined;
+    this.connectButton.name = name ?? '...';
+  }
+
+  enableConnectButton(name?: string, cb?: () => void) {
+    this.connectButton.callback = cb ?? this.onConnectClick.bind(this);
+    this.connectButton.name = name ?? 'Connect';
+  }
+
+  onExecute() {}
 }
-
-
